bd887c7fbfee51acb5fa11d64ef64f318adc8c86
Biaksang Munsong <munsongbiaksang@gmail.com>
2020-09-08 21:16:28 +0530

    Test Version 1 (Express static middleware corrected)

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     next()
 })
-app.use(express.static("frontend/build"))
+app.use(express.static("./public"))
 
 
 // handle routes
